be60eb568b797daaf2b9b624f9ed9a4b28ac9ef0
Lavisse Wchj <66892433+Lavianit@users.noreply.github.com>
2026-04-01 20:15:58 +0800

    Comments

--- AdditionFunctionAddition.py
+++ AdditionFunctionAddition.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[85]:
-
-
 # placeholder, say x and y are the userinputs
 x = 12.4321
 y = 13.654321
@@ -12,24 +6,27 @@ x1 = 12
 x2 = 4321
 y1 = 13 
 y2 = 654321
+
 def Add(x1, x2, y1, y2):
+    # calculation of whole part
     c1 = x1 + y1
-    print(c1)
-    # calculating decimal numbers
-    # Match the fractional parts for calculating
+
+    # calculation of fractional part, match their decimal place
+
     if len(str(x2)) < len(str(y2)):
         x2 *= 10**(len(str(y2))-len(str(x2)))
     elif len(str(x2)) > len(str(y2)):
         y2 *= 10**(len(str(x2))-len(str(y2)))
 
-    ODL = len(str(x2)) # Original Decimal Length, saved for later
+    ODL = len(str(x2)) # Original Decimal Length, saved for later, (x2 can be y2, whichever is fine)
 
     c2 = x2 + y2
-    PreOutputc2 = str(c2) # turn c2 into a string for slicing
-    if len(str(c2)) > ODL: #Carry-over logic, e.g. 0.6 + 0.6 = 1.2 and NOT 0.12
+    PreOutputc2 = str(c2) # turn c2 into a string for slicing when carryover happens
+    
+    if len(str(c2)) > ODL: # carryover has happened and c2 is longer than x2/y2
         Fixedc2 = PreOutputc2[1:]
         Fixedc1 = str(c1 + 1)
-    else:
+    else:                  # carryover has NOT happened and c2 is NOT longer than x2/y2
         Fixedc2 = PreOutputc2
         Fixedc1 = str(c1)
     return(Fixedc1, Fixedc2)
