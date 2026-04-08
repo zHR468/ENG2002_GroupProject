3b4181c80b5e0c730ed84e30e96d37d7f93cf34f
zHR468 <18774878768@163.com>
2026-04-08 12:47:12 +0800

    Add files via upload

---
+++ Task0/Part1/fixnum_module.py
@@ -0,0 +1,32 @@
+class fixNum:
+
+    """
+    The core idea is to represent the number as a fixed-point number with a certain number of fractional digits.
+    In function,
+    a will be the integer part,
+    and b will be the fractional part.
+    b_len stores how many digits b has (for aligning decimals when adding later).
+    """
+    
+    def __init__(self, a, b):
+        # force a and b to be integers
+        self.a = int(a)
+        self.b = int(b)
+
+        # length of b as a digit string (for decimal alignment)
+        self.b_len = len(str(abs(self.b)))
+
+        # In the assignment: if a is non-zero, b must be positive.
+        if self.a != 0:
+            self.b = abs(self.b)
+
+    def __str__(self):
+        # format will be "a.b"
+        a_part = str(self.a)
+        b_part = str(abs(self.b))
+
+        # catch the special negative zero case (e.g., -0.48)
+        if self.a == 0 and self.b < 0:
+            return "-" + a_part + "." + b_part
+        else:
+            return a_part + "." + b_part
